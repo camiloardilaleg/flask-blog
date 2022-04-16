61c20ff523056c28ee7399c0baddbcbf5eeec662
camiloardilaleg <juliancamilo07@hotmail.com>
2022-04-16 18:35:03 -0500

    Main routes defined. Templates correctly set up, but code are redundant. Next step create a base_template.html and change code of others (secundary) templates. i.e. post_form.html

--- run.py
+++ run.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Variables
+posts = []
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello World ARDILA!'
+def index():
+    """Flask busca por defecto las plantillas en la carpeta llamada 'templates' """
+    return render_template('index.html', num_posts=len(posts))
+
+@app.route('/p/<string:slug>/') #Es buena practica acabar con "/" al final de las rutas. 
+def show_post(slug):
+    return render_template('post_view.html', slug_title=slug)
+    
+@app.route("/admin/post/")
+@app.route("/admin/post/<int:post_id>/")
+def post_form(post_id=None):
+    return render_template('admin/post_form.html', post_id=post_id)
