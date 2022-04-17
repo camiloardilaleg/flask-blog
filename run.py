9a0a4a5b0f8c7dd679594330baccb59b1606d323
camiloardilaleg <juliancamilo07@hotmail.com>
2022-04-16 19:17:27 -0500

    Base template created and used in secondary templates. Form created but validations aren't. Next step, create validations by using Flask-WTF.

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from crypt import methods
+from flask import Flask, render_template, redirect, request, url_for
+
 
 app = Flask(__name__)
 
@@ -19,3 +21,16 @@ def show_post(slug):
 @app.route("/admin/post/<int:post_id>/")
 def post_form(post_id=None):
     return render_template('admin/post_form.html', post_id=post_id)
+
+@app.route("/signup/", methods=['GET', 'POST'])
+def show_signup_form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        next = request.args.get('next', None)
+        if next:
+            return redirect(next)
+        return redirect(url_for('next'))
+    return render_template('signup_form.html')
