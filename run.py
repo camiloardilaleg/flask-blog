876e035c478a2147fead8abfd780c15003cea811
camiloardilaleg <juliancamilo07@hotmail.com>
2022-04-21 18:49:51 -0500

    Logging in proccess configurated with flask-login extension/library. Unusefull code at the very beginning of 'run.py' file has been deleted. Lesson 5 is comming

--- run.py
+++ run.py
@@ -1,14 +1,29 @@
-from crypt import methods
-from email.policy import default
 from flask import Flask, render_template, redirect, request, url_for
-from forms import SignupForm, PostForm
+from flask_login import LoginManager, logout_user, current_user, login_user, login_required
+from werkzeug.urls import url_parse
 
+# Own modules
+from forms import SignupForm, PostForm, LoginForm
+from models import users, get_user, User
 
 app = Flask(__name__)
+# Creamos un token para instancia del formulario 
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+login_manager = LoginManager(app)
+login_manager.login_view = "login" # <-- Define la vista que se va a mostrar cuando el usuario no esté logueado
+
 
 # Variables
 posts = []
+
+
+#manejo de inicio de sesion
+@login_manager.user_loader
+def load_user(user_id): #<- es un callback, se puede entender como un middleware 
+    for user in users:
+        if user.id == int(user_id):
+            return user
+    return None
 
 
 @app.route('/')
@@ -22,6 +37,7 @@ def show_post(slug):
     
 @app.route("/admin/post/", methods=['GET', 'POST'], defaults={'post_id': None})
 @app.route("/admin/post/<int:post_id>/", methods=['GET', 'POST'])
+@login_required # <- se protege la vista
 def post_form(post_id):
     form = PostForm()
     print(post_id)
@@ -37,16 +53,47 @@ def post_form(post_id):
         return redirect(url_for('index'))
     return render_template("admin/post_form.html", form=form, post_id=post_id)
 
+
 @app.route("/signup/", methods=['GET', 'POST'])
 def show_signup_form():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form =  SignupForm()
     if form.validate_on_submit():
         name = request.form['name']
         email = request.form['email']
         password = request.form['password']
 
-        next = request.args.get('next', None)
-        if next:
-            return redirect(next)
-        return redirect(url_for('next'))
+        #Create and save new user
+        user = User(len(users) + 1, name, email, password)
+        users.append(user)
+
+        #Keep new user logged in
+        login_user(user, remember=True)
+
+        next_page = request.args.get('next', None)
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('signup_form.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # current_user viene desde flask_login
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = get_user(form.email.data)
+        if user is not None and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('index')
+            return redirect(next_page)
+    return render_template('login_form.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
